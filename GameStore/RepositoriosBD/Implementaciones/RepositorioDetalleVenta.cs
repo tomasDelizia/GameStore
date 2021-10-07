74f7c7d582adbacef8b5bc1066893b714b698949
Gaston Mastrapasqua <gastimastra@gmail.com>
2021-10-07 17:31:27 -0300

    carga proveedor

---
+++ GameStore/RepositoriosBD/Implementaciones/RepositorioDetalleVenta.cs
@@ -0,0 +1,16 @@
+﻿using GameStore.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.RepositoriosBD.Implementaciones
+{
+    public class RepositorioDetalleVenta : Repositorio<DetalleVenta>
+    {
+        public RepositorioDetalleVenta(ContextoGameStore contextoBd) : base(contextoBd)
+        {
+        }
+    }
+}
---
+++ GameStore/Servicios/Implementaciones/ServicioDetalleVenta.cs
@@ -0,0 +1,17 @@
+﻿using GameStore.Entidades;
+using GameStore.RepositoriosBD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Servicios.Implementaciones
+{
+    public class ServicioDetalleVenta : Servicio<DetalleVenta>
+    {
+        public ServicioDetalleVenta(IRepositorioDetalleVenta repositorio) : base(repositorio)
+        {
+        }
+    }
+}
