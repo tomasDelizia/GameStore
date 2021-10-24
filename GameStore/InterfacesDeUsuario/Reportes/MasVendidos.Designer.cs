26f50a2098a99f53231f57050949b66fc3581620
Tomás Delizia <tomidelizia@gmail.com>
2021-10-24 20:40:19 -0300

    Reportes más vendidos añadido

--- GameStore/InterfacesDeUsuario/Reportes/MasVendidos.Designer.cs
+++ GameStore/InterfacesDeUsuario/Reportes/MasVendidos.Designer.cs
@@ -39,14 +39,14 @@ namespace GameStore.InterfacesDeUsuario.Reportes
             this.RwMasVendidos.Location = new System.Drawing.Point(0, 0);
             this.RwMasVendidos.Name = "RwMasVendidos";
             this.RwMasVendidos.ServerReport.BearerToken = null;
-            this.RwMasVendidos.Size = new System.Drawing.Size(833, 903);
+            this.RwMasVendidos.Size = new System.Drawing.Size(833, 749);
             this.RwMasVendidos.TabIndex = 0;
             // 
             // MasVendidos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(833, 903);
+            this.ClientSize = new System.Drawing.Size(833, 749);
             this.Controls.Add(this.RwMasVendidos);
             this.Name = "MasVendidos";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
